2e959c066b8ac1085cc4560470eabbaaa9099c4d
jplf <lefevre@fonteny.org>
2014-12-30 21:12:07 +0100

    French version

---
+++ www/kontrol/server/cmd-fr.js
@@ -0,0 +1,38 @@
+//__________________________________________________________________________
+/**
+ * Fonteny javascript library - December 2014
+
+ * This software is governed by the
+ * Gnu general public license (http://www.gnu.org/licenses/gpl.html)
+
+ * This is the dictionnary used by the french version of the application.
+ *
+ * @author Jean-Paul Le Fèvre <lefevre@fonteny.org>
+*/
+//__________________________________________________________________________
+
+"use strict";
+
+module.exports = {
+    /**
+     * Words leading to the same action are grouped in list of
+     * synonyms.
+     */
+    language:  'french',
+
+    yesList:   ['oui', 'dac', 'absolument'],
+    noList:    ['non', 'refus'],
+    plusList:  ['plus', 'fort', 'fortement', 'augmenter', 'augmenté',
+                'plus haut'],
+    minusList: ['moins', 'doux', 'doucement', 'diminuer', 'diminué',
+                'plus bas'],
+    workList:  ['jouer', 'commencer', 'allumer',
+                'joué', 'commencé', 'allumé','musique', 'radio'],
+    stopList:  ['stop', 'terminer', 'terminé', 'silence', 'éteindre', 'finir'],
+    digitList: ['zero', 'un', 'deux', 'trois', 'quatre', 'cinq',
+                     'six', 'sept', 'huit', 'neuf', 'dix'],
+    indexList: ['premier', 'précédent', 'suivant', 'dernier'],
+    whichList: ['lequel', 'laquelle', 'selection', 'station', 'poste'],
+    setList:   ['alarme', 'état', 'réveil']
+}
+//__________________________________________________________________________
