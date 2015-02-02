0a9bfd57c867cd0b53a196de1c598d218f966f96
jplf <lefevre@fonteny.org>
2015-02-02 17:38:40 +0100

    List of words fixed

--- www/kontrol/server/cmd-fr.js
+++ www/kontrol/server/cmd-fr.js
@@ -5,7 +5,7 @@
  * This software is governed by the
  * Gnu general public license (http://www.gnu.org/licenses/gpl.html)
 
- * This is the dictionnary used by the french version of the application.
+ * This is the dictionary used by the french version of the application.
  * Actually it has been only tested with the remote version of the voice
  * recognition program based on the google api.
  *
@@ -19,26 +19,26 @@ module.exports = {
     /**
      * Words leading to the same action are grouped in list of
      * synonyms.
+     * Some lists are actually in use.
      */
     language:  'french',
 
     yesList:   ['oui', 'dac', 'absolument'],
     noList:    ['non', 'refus'],
-    plusList:  ['plus', 'fort', 'fortement', 'augmenter', 'augmenté',
-                'plus fort', 'plus haut'],
-    minusList: ['moins', 'doux', 'doucement', 'diminuer', 'diminué',
-                'plus bas', 'moins fort'],
+    plusList:  ['plus fort', 'fortement', 'augmenter', 'augmenté',
+                'plus haut'],
+    minusList: ['moins fort', 'doucement', 'diminuer', 'diminué',
+                'plus bas'],
     workList:  ['musique', 'jouer', 'commencer', 'allumer',
-                'joué', 'commencé', 'allumé','radio'],
+                'joué', 'commencé', 'allumé', 'radio'],
     stopList:  ['silence', 'stop', 'terminer', 'terminé', 'éteindre', 'finir',
                 'arrêter', 'arrêté', , 'arrêt'],
-    digitList: ['zero', 'un', 'deux', 'trois', 'quatre', 'cinq',
-                'six', 'sept', 'huit', 'neuf', 'dix'],
+    digitList: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
     indexList: ['premier', 'précédent', 'suivant', 'dernier',
                 'première', 'précédente', 'suivante', 'dernière'],
-    whichList: ['lequel', 'laquelle', 'selection', 'station', 'poste'],
+    whichList: ['laquelle', 'lequel', 'selection', 'station', 'poste'],
     setList:   ['alarme', 'état', 'réveil'],
-    nameList:  ['Fip Radio', 'France Inter', 'France Musique', 'France Culture',
-                'Chante France', 'TSF Jazz']
+    nameList:  ['Fip radio', 'France Inter', 'France Musique', 'France Culture',
+                'chante France', 'TSF Jazz']
 }
 //__________________________________________________________________________
