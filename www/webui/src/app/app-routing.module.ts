4d797c4d58659a93be478517e5b1a2f39650f6ee
lefevre <jean-paul.lefevre@cea.fr>
2020-07-27 19:06:07 +0200

    Still building the app UI

--- www/webui/src/app/app-routing.module.ts
+++ www/webui/src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 import { RadioComponent } from './radio/radio.component';
 import { StateComponent } from './state/state.component';
+import { TriggerComponent } from './trigger/trigger.component';
 
 const routes: Routes = [
-    { path: 'radio', component: RadioComponent },
-    { path: 'state', component: StateComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'trigger', component: TriggerComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- www/webui/src/app/app.module.ts
+++ www/webui/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { StationComponent } from './station/station.component';
 import { RadioComponent } from './radio/radio.component';
 import { StateComponent } from './state/state.component';
+import { TriggerComponent } from './trigger/trigger.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StationComponent,
     RadioComponent,
-    StateComponent
+    StateComponent,
+    TriggerComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
